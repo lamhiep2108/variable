f699e1df227457c528640c6abc502011971d78a7
Admin <hiepnothip@gmail.com>
2021-01-08 17:16:42 +0700

    'l'

---
+++ src/firstdegreeequation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class firstdegreeequation {
+    public static void main(String[] args) {
+        System.out.println("linear Equation Resolver");
+        System.out.println("Giver o equation as 'a*x+b=0' please enter constant");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+        System.out.println("c:");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("the solution is: %f !", solution);
+        }
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("equation pass with %f!/n", answer);
+        } else {
+            if (b==0) {
+                System.out.print("the solution is all x!");
+            }else {
+                System.out.print("no solution");
+            }
+        }
+    }
+}
